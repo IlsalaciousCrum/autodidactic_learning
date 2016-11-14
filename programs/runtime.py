9a6383d7a2738ca2b5f3dcfa3681c05e954a5d23
Ilsa Gordon <ilsalacious@gmail.com>
2016-11-14 22:05:50 +0000

    created html template
    Working route and html template. Javacript for buttons and toggling the
    answer not yet working.

---
+++ programs/runtime.py
@@ -0,0 +1,39 @@
+import random
+
+
+def runtime_flashcards():
+    "Returns a random runtime question and answer from a dictionary"
+
+    runtime_dict = {"What is runtime or runtime complexity?": "How quickly the time it takes to run a program grows relative to the input, as the input gets arbitrarily large.",
+                    "What is O(1) called?": "Constant time",
+                    "What is O(log n) called?": "Logarithmic time",
+                    "What is O(n) called?": "Linear runtime",
+                    "What is O(n log n) called?": "Loglinear time (a more complex logarithmic runtime)",
+                    "What is O(n^2) called?": "Quadratic time",
+                    "What is O(2^N) called?": "Exponential time",
+                    "What is O(n!) called?": "Factorial time",
+                    "Define quadratic": "Involving the second and no higher power of an unknown quantity or variable.",
+                    "What is asymptotic behavior?": "The shape that a graph takes as it's line approaches infinity. Big O Notation is a type of asymptotic analysis",
+                    "What are the runtimes in order of best to worst?": ["Constant time - O(1)", "Logarithmic time - O(log n)", "Linear time - O(n)", "Also logarithmic runtime - O(n log n)", "Quadratic time - O(n^2)", "Also exponential - O(2^n)", "Factorial time - O(n!)"],
+                    "What are the runtimes in order of worst to best?": ["Factorial time - O(n!)", "Also exponential - O(2^n)", "Quadratic time - O(n^2)", "Also logarithmic runtime - O(n log n)", "Linear time - O(n)", "Logarithmic time - O(log n)", "Constant time - O(1)"],
+                    "Tell me about O(n)": "Linear runtime means that if your list is n long, it takes n time to do your calculations.",
+                    "Tell me about O(1)": "Constant time means that the program will take the same time no matter how large the input.",
+                    "Tell me about O(n!)": "Factorial time involves doing something for all possible permutations of the n elements. It is impractical for any reasonably large input size n.",
+                    "Tell me about O(2^n)": "Exponential time denotes an algorithm whose growth doubles with each additon to the input data set. The operation is impractical for any reasonably large input size n.",
+                    "Tell me about O(log n)": "Logarithmic time means that at every step the work left to check is halved. Logarithmic behavior is typically in a program that divides the input size every step. This could be something like binary search, or an update condition in a for loop",
+                    "Tell me about O(n log n)": "Loglinear runtime means that you're performing an O(log n) operation for each item in your input. Most (efficient) sort algorithms are an example of this. Examples are Quicksort and Mergesort. Increasing the input size hurts, but may still be manageable. Every time you double n, you spend twice as much time plus a little more. The operation is only really practical up to a certain input size. Every time n doubles, the operation takes four times as long.",
+                    "Tell me about O(n^2)": "Quadratic runtime means that for every element, you do something with every other element, such as comparing them. Such as when you have a recursive function that makes multiple calls, the runtime will often, but not always, look like O(branchesdepth). An example would be password cracking.",
+                    "Describe what a graph of constant time would look like?": "Flat",
+                    "Which is more desirable, a steep curve or a flat curve?": "Flat",
+                    "Give an example of O(1)": "Constant time example: Dictionary lookup or array indexing, Determining if a number is even or odd. Using a constant-size lookup table or hash table.",
+                    "Give an example of O(n)": "Linear runtime example: a single for loop or finding an item in an unsorted list.",
+                    "Give an example of O(n^2)": "Quadratic time example: two nested for loops, bubble sort, insertion sort",
+                    "Give an example of O(n!)": "Factorial time example: Graph traversal. Traveling Salesperson problem",
+                    "Give an example of O(log n)": "Logarithmic time example: finding an item in a sorted list with a balanced search tree or a binary search",
+                    "Give an example of O(n log n)": "Loglinear runtime example: quicksort (in the average and best case), heapsort and merge sort.",
+                    "Give an example of O(2^n)": "Exponential runtime example: if you wanted to break an encryption scheme and you didn't know of any weakness in the encryption scheme. O(2^N) (exponential time) is really bad, which is why strong encryption is so hard to break using a bruteforce approach."
+                    }
+
+    return random.sample(runtime_dict.items(), 1)
+
+runtime_flashcards()
--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from programs.runtime import runtime_flashcards
 
 import os
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
 app.secret_key = os.environ['APP_SECRET_KEY']
+
+app.jinja_env.undefined = StrictUndefined
 
  # ___________________________________
 
 
 @app.route('/hello')
 def say_hello():
+    """The landing page for this series of learning exercises"""
+
     html = "<html><body>This is where I work on learning</body></html>"
     return html
 
+
+@app.route('/runtime')
+def runtime():
+    """Flashcards for learning runtime"""
+
+    flashcard = runtime_flashcards()
+
+    return render_template("runtime.html", flashcard=flashcard)
 
 #  ____________________________________
 
